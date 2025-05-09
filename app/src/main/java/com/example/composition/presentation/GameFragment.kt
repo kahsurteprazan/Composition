bd4514509e4d9ef98f795e004bc422365eb567e6
HALOPENE <jebory@mail.ru>
2025-05-09 14:27:20 -0400

    Create data and presentation flow

---
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -0,0 +1,33 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+
+class GameFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            GameFragment().apply {
+
+            }
+    }
+}
